332c837efe83382da37c1f2f24e11ee71a9ab8e6
sergey petrachkov <petrachkovsergey@gmail.com>
2018-08-15 20:45:42 +0700

    [wip] - exporting black screen without audio so far

--- PreUploadVideoProcessor/ProcessingQueue/VideosListController.swift
+++ PreUploadVideoProcessor/ProcessingQueue/VideosListController.swift
@@ -9,6 +9,8 @@
 import Foundation
 import UIKit
 import AVFoundation
+import AVKit
+import Photos
 
 
 class WCITNavigationController: UINavigationController {
@@ -51,14 +53,15 @@ extension UIViewController {
 }
 
 
-class VideosListController: UITableViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class VideosListController: UITableViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, SDAVAssetExportSessionDelegate, AssetExportSessionDelegate {
   
   let imagePickerController: UIImagePickerController = {
     let controller = UIImagePickerController()
     controller.sourceType = .photoLibrary
     controller.mediaTypes = ["public.movie"]
     controller.allowsEditing = false
-    controller.videoExportPreset = AVAssetExportPreset640x480
+    controller.videoExportPreset = AVAssetExportPreset960x540
+    
     return controller
   }()
   
@@ -66,6 +69,29 @@ class VideosListController: UITableViewController, UIImagePickerControllerDelega
     super.viewDidLoad()
     self.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(self.didTapPlus))
     self.imagePickerController.delegate = self
+    self.authorizeToAlbum(completion: { result in })
+  }
+  func authorizeToAlbum(completion:@escaping (Bool)->Void) {
+    
+    if PHPhotoLibrary.authorizationStatus() != .authorized {
+      NSLog("Will request authorization")
+      PHPhotoLibrary.requestAuthorization({ (status) in
+        if status == .authorized {
+          DispatchQueue.main.async(execute: {
+            completion(true)
+          })
+        } else {
+          DispatchQueue.main.async(execute: {
+            completion(false)
+          })
+        }
+      })
+      
+    } else {
+      DispatchQueue.main.async(execute: {
+        completion(true)
+      })
+    }
   }
   
   @objc private func didTapPlus() {
@@ -74,22 +100,60 @@ class VideosListController: UITableViewController, UIImagePickerControllerDelega
   
   
   func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+    
     if let videoURL = info[UIImagePickerControllerMediaURL] as? URL {
+//      DispatchQueue.main.async {
+        picker.dismiss(animated: true, completion: nil)
+//      }
+      
       let tempDirectory = NSTemporaryDirectory()
       let processedURL = URL(fileURLWithPath: tempDirectory.appending(UUID().uuidString).appending(".mp4"))
-      print("\(videoURL) \(processedURL)")
+      
+      let asset = AVAsset(url: videoURL)
+      
+      let encoder = AssetExportSession(asset: asset,
+                                       outputUrl: processedURL,
+                                       outputFileType: .mp4,
+                                       videoSettings: [
+                                        AVVideoCodecKey: AVVideoCodecType.h264,
+                                        AVVideoWidthKey: 600,
+                                        AVVideoHeightKey: 400],
+                                       audioSettings: [AVFormatIDKey: kAudioFormatMPEG4AAC, AVNumberOfChannelsKey: 2, AVSampleRateKey: 44100, AVEncoderBitRateKey: 128000])
+      encoder.delegate = self
+      
+      do {
+        try encoder.exportAsynchronously {
+            print(encoder.getStatus())
+          DispatchQueue.main.async {
+            PHPhotoLibrary.shared().performChanges({
+              PHAssetChangeRequest.creationRequestForAssetFromVideo(atFileURL: processedURL)
+            }) { saved, error in
+              if saved {
+                let alertController = UIAlertController(title: "Your video was successfully saved", message: nil, preferredStyle: .alert)
+                let defaultAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+                alertController.addAction(defaultAction)
+                self.present(alertController, animated: true, completion: nil)
+              }
+            }
+          }
+//
+
+        }
+      } catch let error {
+        print("\(error)")
+      }
     }
-//    // Handle movie capture
-//    NSURL *movieURL = [info objectForKey:
-//      UIImagePickerControllerMediaURL];
-//
-//    NSURL *uploadURL = [NSURL fileURLWithPath:[[NSTemporaryDirectory() stringByAppendingPathComponent:[self randomString]] stringByAppendingString:@".mp4"]];
-//
-//    // Compress movie first
-//    [self convertVideoToLowQuailtyWithInputURL:movieURL outputURL:uploadURL];
   }
   
   func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
     picker.dismiss(animated: true, completion: nil)
+  }
+  
+  func exportSession(_ exportSession: AssetExportSession?, renderFrame pixelBuffer: CVPixelBuffer?, withPresentationTime presentationTime: CMTime, to renderBuffer: CVPixelBuffer?) {
+    print(exportSession)
+  }
+  
+  func exportSession(_ exportSession: SDAVAssetExportSession!, renderFrame pixelBuffer: CVPixelBuffer!, withPresentationTime presentationTime: CMTime, to renderBuffer: CVPixelBuffer!) {
+    print(exportSession)
   }
 }
